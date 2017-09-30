60c67aaf30b539c0279e37e4b47faf7b9cb47e84
T.J. Refugio <trefug2@uic.edu>
2017-09-29 21:33:11 -0500

    <TJ> Added New Servo Sweep Code
    >9:31pm 09/29/2017
    > Added a new "servo sweep" arduino code: basically the same as "Servo_basic" except I added a technical note, and the servo input pin is now a variable.

---
+++ Arduino/Servo_basic/Servo_Sweep_ver2.ino
@@ -0,0 +1,32 @@
+/*Note: Technically, the servo is only "writing" to angles 0 and 180,
+But to get from 0 to 180, the servo has to hit all angles, hence it gives the illusion of
+"writing" to all angles. Same idea goes for 180 to 0. 
+*/
+
+#include <Servo.h>
+
+int position = 0;
+
+Servo myservo; // Creating a Servo object
+int servoPin = 9; // Servo Input Pin
+
+void setup() {
+
+  myservo.attach(servoPin); // Attaching pin 13 to myservo object
+}
+
+void servo_sweep(int pos, int pause){ // Sweeps to a position and exits after a delay;
+  myservo.write(pos);
+  delay(pause);
+  return;
+}
+
+
+void loop() {
+
+  servo_sweep(0,800);
+  servo_sweep(180,800);
+
+}
+
+
